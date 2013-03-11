d4202417faac8c2e8faa048e40d49d5f8209b2a0
Mike Pennisi <mike@mikepennisi.com>
2013-03-11 15:27:23 -0400

    Begin implementation of stack/Account

---
+++ test/public/tests/account.js
@@ -0,0 +1,32 @@
+/* global suite, test, assert, beforeEach */
+define(['opjs/stack/account', 'mocks/peer'], function (Account, PeerMock) {
+  'use strict';
+  suite('Account', function () {
+
+    suite('#addPeer', function () {
+      beforeEach(function () {
+        this.acct = new Account();
+      });
+      test('Returns reference to new peers', function () {
+        var p1 = new PeerMock();
+        var added = this.acct.addPeer(p1);
+
+        assert.equal(added, p1);
+      });
+      test('Returns reference to previously-added peers', function () {
+        var p1 = new PeerMock();
+        var p2 = new PeerMock();
+        var added1, added2;
+
+        p2.$setPeerURI(p1.getPeerURI());
+
+        added1 = this.acct.addPeer(p1);
+        added2 = this.acct.addPeer(p2);
+
+        assert.equal(added1, added2);
+        assert.notEqual(added2, p2);
+        assert.equal(added2.getPeerURI(), p2.getPeerURI());
+      });
+    });
+  });
+});
