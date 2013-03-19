81029546f305f327a8e87c910f6ef98ada89b244
Tim Caswell <tim@creationix.com>
2013-03-19 09:57:20 -0500

    Add basic node server with websockets

---
+++ prototype/server.js
@@ -0,0 +1,34 @@
+
+var http = require('http');
+var urlParse = require('url').parse;
+var pathJoin = require('path').join;
+var send = require('send');
+var WebSocketServer = require('ws').Server;
+
+var server = http.createServer(function (req, res) {
+  var pathname = urlParse(req.url).pathname;
+
+  if (pathname.slice(0, 6) === '/opjs/') {
+    send(req, pathname.slice(5))
+      .root(pathJoin(__dirname, '..', 'lib'))
+      .pipe(res);
+    return;
+  }
+
+  send(req, pathname)
+    .root(pathJoin(__dirname, 'public'))
+    .pipe(res);
+});
+
+var wsServer = new WebSocketServer({server: server});
+wsServer.on('connection', function (socket) {
+  socket.on('message', function (message) {
+    socket.send(message);
+  });
+  socket.send('Hello');
+});
+
+server.listen(8080, function () {
+  console.log('Server listening at', server.address());
+});
+
