d420465c99412852ace9af7a44478047c94afa24
Christoph Dorn <christoph@christophdorn.com>
2013-04-16 10:59:57 -0700

    account finder live test

---
+++ dev/tests-browser/stack/07-AccountFinder-Live.js
@@ -0,0 +1,26 @@
+/* global define, suite, test, assert */
+define([
+  'opjs/Stack'
+], function (Stack) {
+
+  'use strict';
+
+  suite('AccountFinder-Live', function () {
+/*
+    test('connect', function(done) {
+      var client = new Stack({
+        context: {
+          logPrefix: "AccountFinder-Live - connect"
+        },
+        "domain": "unstable.hookflash.me"
+      });
+      return client.ready().then(function() {
+        return client.destroy().then(function() {
+          return done(null);
+        });
+      }).fail(done);
+    });
+*/
+  });
+
+});
