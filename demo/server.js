d286bd0e1efa1a98f0f0869c72dc11f9415a3a32
Christoph Dorn <christoph@christophdorn.com>
2013-06-11 16:06:55 -0700

    only start server if run as main

--- demo/server.js
+++ demo/server.js
@@ -63,39 +63,41 @@ function handler(req, res) {
     });
 }
 
-// Start the server(s)
-// Run in production mode if the process is root
-var server = http.createServer(handler);
 
-server.listen(process.getuid() ? 8080 : 80);
-console.log('HTTP server listening at', server.address());
+if (module === require.main) {
+    // Start the server(s)
+    // Run in production mode if the process is root
+    var server = http.createServer(handler);
 
-// If a certificate file exists, start an https server too
-var pfx;
-try {
-    pfx = require('fs').readFileSync('certificate.pfx');
+    server.listen(process.getuid() ? 8080 : 80);
+    console.log('HTTP server listening at', server.address());
+
+    // If a certificate file exists, start an https server too
+    var pfx;
+    try {
+        pfx = require('fs').readFileSync('certificate.pfx');
+    }
+    catch (err) {
+
+    }
+    if (pfx) {
+        server = https.createServer({
+            pfx: pfx,
+            honorCipherOrder: true
+        }, handler);
+
+        server.listen(process.getuid() ? 8443 : 443);
+        console.log('HTTPS server listening at', server.address());
+    }
+
+    //drop to normal user after binding to the port
+    if (!process.getuid()) {
+    //  If we're running as root, drop down to a regular user after binding to 80
+        var stat = require('fs').statSync(__filename);
+
+        console.log('Dropping to normal user', {gid: stat.gid, uid: stat.uid});
+
+        process.setgid(stat.gid);
+        process.setuid(stat.uid);
+    }
 }
-catch (err) {
-
-}
-if (pfx) {
-    server = https.createServer({
-        pfx: pfx,
-        honorCipherOrder: true
-    }, handler);
-
-    server.listen(process.getuid() ? 8443 : 443);
-    console.log('HTTPS server listening at', server.address());
-}
-
-//drop to normal user after binding to the port
-if (!process.getuid()) {
-//  If we're running as root, drop down to a regular user after binding to 80
-    var stat = require('fs').statSync(__filename);
-
-    console.log('Dropping to normal user', {gid: stat.gid, uid: stat.uid});
-
-    process.setgid(stat.gid);
-    process.setuid(stat.uid);
-}
-
